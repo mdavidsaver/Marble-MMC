c45134815f9c8ea3576eb5d97c97524b966a02f1
Michael Davidsaver <mdavidsaver@gmail.com>
2026-01-15 15:47:54 -0800

    fans default to full speed

--- inc/eeprom.h
+++ inc/eeprom.h
@@ -59,7 +59,7 @@ typedef struct {
   X(1, boot_mode, raw, 1, {0}) \
   X(2, mac_addr,  mac, 6, {18, 85, 85, 0, 1, 46}) \
   X(3, ip_addr,   ip4, 4, {192, 168, 19, 31}) \
-  X(4, fan_speed, raw, 1, {102}) \
+  X(4, fan_speed, raw, 1, {120}) \
   X(5, overtemp,  raw, 1, {85}) \
   X(6, mgt_mux,   raw, 1, {0}) \
   X(7, fsynth,    raw, 6, {0, 0, 0, 0, 0, 0}) \
